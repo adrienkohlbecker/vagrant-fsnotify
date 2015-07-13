1cfcf53851755f210a4ee6a79234418f5e665a38
Leandro Facchinetti <me@leafac.com>
2015-07-13 10:08:23 -0400

    Update to latest listen
    Reference: https://github.com/mitchellh/vagrant/commit/d5458247c7490f0eff79d3e39679a22c5d67ae81

--- lib/vagrant-fsnotify/plugin.rb
+++ lib/vagrant-fsnotify/plugin.rb
@@ -4,8 +4,12 @@ rescue LoadError
   raise "The vagrant-fsnotify plugin must be run within Vagrant."
 end
 
-if Vagrant::VERSION < "1.5"
-  raise "The vagrant-fsnotify plugin is only compatible with Vagrant 1.5+"
+if Vagrant::VERSION < "1.7.3"
+  raise <<-ERROR
+The vagrant-fsnotify plugin is only compatible with Vagrant 1.7.3+. If you can't
+upgrade, consider installing an old version of vagrant-fsnotify with:
+  $ vagrant plugin install vagrant-fsnotify --plugin-version 0.0.6.
+ERROR
 end
 
 module VagrantPlugins::Fsnotify
